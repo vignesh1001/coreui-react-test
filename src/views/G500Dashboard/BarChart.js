3435d820c4f853d906dee63382425113ad7b9dc5
vignesh1001 <vignesh1001@gmail.com>
2020-02-16 08:33:04 +0530

    2 files modified

--- src/views/G500Dashboard/BarChart.js
+++ src/views/G500Dashboard/BarChart.js
@@ -14,16 +14,17 @@ import {
   DropdownToggle,
   Table
 } from "reactstrap";
-import './TotalRevenue.scss';
-import {Bar} from 'react-chartjs-2';
-import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import "chartjs-plugin-datalabels";
+import "./TotalRevenue.scss";
+import { Bar, Chart } from "react-chartjs-2";
+import { CustomTooltips } from "@coreui/coreui-plugin-chartjs-custom-tooltips";
 
 const styles = {
   header: {
     fontSize: 22,
     letterSpacing: 0,
     opacity: 1
-  },
+  }
 };
 
 class BarChart extends React.Component {
@@ -32,92 +33,111 @@ class BarChart extends React.Component {
     this.state = {
       isWeekDDOpen: false,
       weekDDValue: "This Week",
-      line : {
-        labels: ['12/9','', '12/10','', '12/11','', '12/13','', '12/14', '12/15'],
+      data: {
+        labels: ["DIESEL","DIESEL","DIESEL","REGULAR","PREMINUM"],
         datasets: [
           {
-            label: 'My First dataset',
-            data: [10,1100,1025,2110,1900,2000,900,1000,900,2025],
-            backgroundColor: 'rgba(152, 152, 208, 0.2)',
-            borderColor: 'rgba(19, 247, 228,1)',
+            type: "bar",
+            label: "My First dataset",
+            data: [83,25,26,67,86],
             borderWidth: 1,
-            pointBackgroundColor: 'rgba(19, 247, 228,1)',
-            pointBorderColor: 'rgba(19, 247, 228,1)',
+            backgroundColor: "#45a973",
             pointBorderWidth: 5,
+            yAxisID: "y-axis-1",
+            datalabels: {
+              color: 'white',
+              formatter: function(value, context) {
+                return context.chart.data.datasets[0].data[context.dataIndex]+'%';
+              }
+            }
           },
-        ],
-      }
+          {
+            label: "Sales",
+            type: "line",
+            data: [10, 10, 10, 10, 10],
+            labelData: ['min\n4,000', 'min\n5,000', 'min\n11,000', 'min\n4,000', 'min\n4,000'],
+            position: "bottom",
+            fill: false,
+            borderColor: "#da9aaa",
+            yAxisID: "y-axis-2",
+            datalabels: {
+              color: 'black',
+              align: 'start',
+              anchor: 'start',
+              offset: -10,
+              formatter: function(value, context) {
+                return context.chart.data.datasets[1].labelData[context.dataIndex];
+              },
+              font:{
+                size:10,
+              },
+              align:'right',
+              padding: {
+                right: 55,
+              }
+            }
+          }
+        ]
+      },
+       options: {
+          scales: {
+            xAxes: [{
+              ticks: {
+                fontColor: '#000',
+                fontStyle: "bold",
+                fontFamily: "Roboto",
+                fontSize: 12,
+              }
+            }],
+            yAxes: [
+              {
+                ticks: {
+                  max: 100,
+                  min: 0,
+                  stepSize: 20,
+                  beginAtZero: true,
+                  callback: function(label, index, labels) {
+                    return label+'%';
+                  }
+                },
+                id: "y-axis-1",
+              },
+              {
+                ticks: {
+                  max: 100,
+                  min: 0,
+                  stepSize: 20,
+                  beginAtZero: true,
+                },
+                display: false,
+                id: "y-axis-2",
+              }
+            ]
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            enabled: false,
+            custom: CustomTooltips
+          },
+        }
     };
   }
 
-  onChangeDropdown(e) {
-    this.setState({weekDDValue: e.currentTarget.textContent});
-  }
-
   render() {
-    const {weekDDValue, isWeekDDOpen,line} = this.state;
-    const plugins = [{
-      afterDraw: (chartInstance, easing) => {
-          const ctx = chartInstance.chart.ctx;
-          ctx.fillText("This text drawn by a plugin", 100, 10);
-          console.log(ctx);
-      }
-  }];
+    const { data, options } = this.state;
     return (
       <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12 pdg-0px">
         <Container className="total_revenue">
           <Container className="total_revenue__body">
             <Card>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Bar 
-                  plugins={plugins}
-                  data={line} 
-                  options={{
-                    scales: {
-                      xAxes: [ {
-                        display: true,
-                        scaleLabel: {
-                          display: true,
-                          labelString: 'Date'
-                        },
-                        ticks: {
-                          userCallback: function(item, index) {
-                            if (!(index % 2)) return item;
-                          },
-                          autoSkipPadding: 30,
-                          major: {
-                            fontStyle: 'bold',
-                            fontColor: '#FF0000'
-                          },
-                        }
-                      } ],
-                      yAxes: [ {
-                        display: true,
-                        scaleLabel: {
-                          display: true,
-                          labelString: 'Liters'
-                        },
-                        ticks: {
-                          max: 3000,
-                          min: 0,
-                          stepSize: 1000,
-                          beginAtZero:true,
-                        }
-                      } ]
-                    },
-                    legend: {
-                      display: false
-                    },
-                    tooltips: {
-                      enabled: false,
-                      custom: CustomTooltips
-                    },
-                    maintainAspectRatio: false,
-                  }} />
-              </div>
-            </CardBody>
-          </Card>
+              <CardBody>
+                <div className="chart-wrapper">
+                  <Bar data={data} options={options} />
+                </div>
+              </CardBody>
+            </Card>
           </Container>
         </Container>
       </div>
